b3609846a7e35c91836e8e7254319efecf9b6f31
romanovAl <ml.naslednik2001@gmail.com>
2020-05-25 21:04:14 +0500

    Исправил косяки

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 template <typename T>
-vector<T> task(vector<T> vectorA) {
+vector<T> task(vector<T> const& vectorA) {
+
 	vector<T> vectorB;
 
-
-	for (int i = 0; i < vectorA.size() - 1; i++) {
+	for (size_t i = 0; i < vectorA.size() - 1; i++) {
 
 		T x = vectorA[i + 1];
 		T y = vectorA[i];
@@ -26,7 +26,7 @@ vector<T> task(vector<T> vectorA) {
 			}
 		}
 		if (x < y) {
-			for (int j = i; j < vectorA.size() - 1; j++) {
+			for (size_t j = i; j < vectorA.size() - 1; j++) {
 				T x1 = vectorA[j + 1];
 				T y1 = vectorA[j];
 				if (x1 > y1) {
@@ -46,11 +46,11 @@ int main() {
 
 	vector<int> vectorA = { 1, 3, 10, 20, 5, 6, 4, 0, -1, -1, -1, 4, 40 };
 	
-	vector<int> vectorB = task(vectorA);
+	vectorA = task(vectorA);
 
 	cout << "b - ";
 
-	for (auto& x : vectorB) {
+	for (auto& x : vectorA) {
 		cout << x << ",";
 	}
 	return 0;
